aa0e3bfd18557ce436299a5663af07831b95396f
Buwanekafernando <fernandodeshan0@gmail.com>
2025-12-30 09:19:24 +0530

    port added

--- backend/config/db.php
+++ backend/config/db.php
@@ -3,10 +3,13 @@ $host = "localhost";
 $user = "root";
 $password = "db@366";
 $database = "bookdb";
+$port = 3307; 
 
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $password, $database, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+
+echo "Database connected successfully";
 ?>
